729512e12e28a329e9dcfa909df8b42ff67cbdba
JochoJTG <jorgetregom671@gmail.com>
2024-03-17 23:52:59 -0600

    PosControl

--- quad_control/src/UAV_Dynamics.cpp
+++ quad_control/src/UAV_Dynamics.cpp
@@ -1,20 +1,19 @@
 #include <ros/ros.h>
 #include <eigen3/Eigen/Dense>
+#include <geometry_msgs/Vector3.h>
 
 
 
-const g = 9.81;
-int uav_weight = 2 //KG
-float thrust, force,tx, ty, tz;
+int g = 9.81;
+int uav_weight = 2; //KG
+float thrust, tx, ty, tz;
 
 float phi,theta,psi;
 
-Eigen::Matrix3f intertias_matrix;
-intertias_matrix << 0.0411, 0, 0
-                    0, .0478, 0
-                    0, 0, .0599;
+float step = 1.0/100.0;
+
+Eigen::Matrix3f inertias_matrix;
 Eigen::Vector3f torques;
-torques << tx, ty, tz;
 
 Eigen::Vector3f angular_accel_body;
 Eigen::Vector3f angular_vel_body;
@@ -24,103 +23,111 @@ Eigen::Vector3f attitude;
 Eigen::Vector3f linear_accel_body; //body frame
 Eigen::Vector3f linear_vel_body; //body frame
 Eigen::Vector3f e3;
-    e3 << 0, 0, 1;
 Eigen::Vector3f linear_pos;
 Eigen::Vector3f linear_vel_inertial;
 Eigen::Matrix3f rotationalmatrix;
 Eigen::Matrix3f rot2;
+Eigen::Vector3f force;
 
-void LinearControlCallback(){
+Eigen::Matrix3f R(float phi, float theta, float psi);
+Eigen::Matrix3f R2(float phi, float theta, float psi);
 
-    thrust = algo->x
+void LinearControlCallback(const geometry_msgs::Vector3::ConstPtr& th){
+
+    thrust = th->x;
 }
 
-void AttitudeControlCallback(){
+void AttitudeControlCallback(const geometry_msgs::Vector3::ConstPtr& moments){
 
-    tx = algo->x;
-    ty = algo->y;
-    tz = algo->z;
+    tx = moments->x;
+    ty = moments->y;
+    tz = moments->z;
 }
 
-void AngularDynamcis(){
 
-        angular_accel_body = intertias_matrix.inverse() * (torques - angular_vel_body.cross(inertias_matrix * angular_vel_body));
+int main(int argc, char **argv){
+
+    ros::init(argc, argv, "Dynamics");
+    ros::NodeHandle nh;
+
+    ros::Publisher Dynamics_pos_Pub = nh.advertise<geometry_msgs::Vector3>("dynamics/pos",10);
+    ros::Publisher Dynamics_vel_Pub = nh.advertise<geometry_msgs::Vector3>("dynamics/lin_vel",10);
+    ros::Publisher Dynamics_attitude_Pub = nh.advertise<geometry_msgs::Vector3>("dynamics/attitude",10);
+    ros::Publisher Dynamics_attitude_dot_Pub = nh.advertise<geometry_msgs::Vector3>("dynamics/attitude_dot",10);
+
+    ros::Subscriber LinearControl_Sub = nh.subscribe("/posdata/angdes", 10, &LinearControlCallback);
+    ros::Subscriber AttitudeControl_Sub = nh.subscribe("/torques", 10, &AttitudeControlCallback);
+
+    ros::Rate loop_rate(100);
+
+    inertias_matrix << 0.0411, 0, 0,
+                        0, .0478, 0,
+                        0, 0, .0599;
+    
+    torques << tx, ty, tz;
+
+    e3 << 0, 0, 1;  
+
+    while (ros::ok())
+    {
+        
+    
+    angular_accel_body = inertias_matrix.inverse() * (torques - angular_vel_body.cross(inertias_matrix * angular_vel_body));
     
 
-    for (uint8_t i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
-        angular_vel_body(i) = angular_vel_body(i) + step + angular_accel_body;
+        angular_vel_body(i) = angular_vel_body(i) + step * angular_accel_body(i);
     }
 
 
-    attitude_dot = R2(attitude(0), attitude(1), attitude(2)) * angular_vel_body
+    attitude_dot = R2(attitude(0), attitude(1), attitude(2)) * angular_vel_body;
 
-    for (uint8_t i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
-        attitude(i) = attitude(i) + step + attitude_dot(i);
+        attitude(i) = attitude(i) + step * attitude_dot(i);
     }
 
-}
 
-void LinearDynamics(){
-
-    force = thrust * e3 + rotationalmatrix.transpose() * uav_weight * g * e3;
+    //Linear dynamics
     
+    force = thrust * e3 + R(attitude(0), attitude(1), attitude(2)).transpose() * uav_weight * g * e3;
+
     linear_accel_body = force/uav_weight - angular_vel_body.cross(linear_vel_body);
     
-
-    for (uint8_t i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
         linear_vel_body(i) = linear_vel_body(i) + step * linear_accel_body(i);
     }
    
     linear_vel_inertial = R(attitude(0), attitude(1), attitude(2)) * linear_vel_body;
     
-    for (uint8_t i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
         linear_pos(i) = linear_pos(i) + step * linear_vel_inertial(i);
     }
 
+
+        ros::spinOnce();
+        loop_rate.sleep();
+
+      
+    }
 }
 
-void R(float phi, float theta, float psi){
+
+Eigen::Matrix3f R(float phi, float theta, float psi){
     rotationalmatrix << cos(psi)*cos(theta), cos(psi)*sin(phi)*sin(theta) - cos(phi)*sin(psi), sin(psi)*sin(phi) + cos(psi)*cos(phi)*sin(theta),
                         cos(theta)*sin(psi), cos(psi)*cos(phi) + sin(psi)*sin(phi)*sin(theta), cos(phi)*sin(psi)*sin(theta) - cos(psi)*sin(phi),
                         -sin(theta)        , cos(theta)*sin(phi)                             , cos(phi)*cos(theta);
+
+    return rotationalmatrix;
 }
-void R2(float phi, float theta, float psi){
+
+Eigen::Matrix3f R2(float phi, float theta, float psi){
     rot2 << 1, sin(phi)*tan(theta), cos(phi)*tan(theta),
             0, cos(phi)           , -sin(phi),
             0, sin(phi)/cos(theta), cos(phi)/cos(theta);
-}
 
-int main(int argc, char **argv){
-
-    ros::init(argc, argv, "Dynamics");
-    ros::nodeHabdle nh;
-
-
-
-    ros::Publisher DynamicsPub = nh.advertise<>("/Dynamics",10);
-    ros::Subscriber LinearControl_Sub = nh.subscriber("/LinearControl", 10, &LinearControlCallback);
-    ros::Subscriber AttitudeControl_Sub = nh.subscriber("/AttitudeControl", 10. &AttitudeControlCallback);
-
-
-    ros::Rate loop_rate(100);
-
-    while (ros::ok())
-    {
-        
-        AngularDynamcis();
-        LinearDynamics();
-
-
-        ros::spinOnce();
-        loop_rate.sleep()
-
-    }
-    
-
-
-
+    return rot2;
 }
